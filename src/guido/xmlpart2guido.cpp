f3319bdaef5931d36618f54c9f79c5279892fc3f
Arshia Cont <arshiacont@antescofo.com>
2025-12-02 11:21:36 +0100

    xml2guido: MuseScore pedal and RehearsalMark

--- src/guido/xmlpart2guido.cpp
+++ src/guido/xmlpart2guido.cpp
@@ -914,9 +914,10 @@ void xmlpart2guido::checkOctavaPendingEnd() {
                             std::string pedalType = element->getAttributeValue("type");
                             if ( (pedalType== "start") || (pedalType == "sostenuto")) {
                                 tag = guidotag::create("pedalOn");
-                                fPreviousPedalYPos = xml2guidovisitor::getYposition(element, 14.0, true);
+                                int kOffset = sIsMuseScore ? 0.0 : 14.0;
+                                fPreviousPedalYPos = xml2guidovisitor::getYposition(element, kOffset, true);
                                 
-                                xml2guidovisitor::addPosY(element, tag, 14.0, 1.0);
+                                xml2guidovisitor::addPosY(element, tag, kOffset, 1.0);
                             }else
                                 if ( (pedalType== "stop") || (pedalType == "discontinue")) {
                                     tag = guidotag::create("pedalOff");
@@ -1301,7 +1302,8 @@ void xmlpart2guido::checkOctavaPendingEnd() {
                                         tag->add (guidoparam::create(s.str(), false));
                                     }
                                 }
-                                xml2guidovisitor::addPosY(element, tag, -4, 1);
+                                int kOffset = sIsMuseScore ? 0 : -4;
+                                xml2guidovisitor::addPosY(element, tag, kOffset, 1);
                                 
                                 add (tag);
                             }
